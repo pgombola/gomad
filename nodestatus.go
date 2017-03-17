f3113a27318cba77c93a3a9cb0dbccdd4185c62c
pgombola <pgombola@pgombola-mbp.extol.com>
2017-03-17 10:13:52 -0400

    Added flags for nomad server and for getting node status

--- nodestatus.go
+++ nodestatus.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/pgombola/gomad/client"
 )
 
+var (
+	nomad string
+	hosts bool
+)
+
+func init() {
+	flag.StringVar(&nomad, "nomad", "", "Host address and port of nomad server")
+	flag.BoolVar(&hosts, "hosts", false, "Retrieve the status of the hosts")
+}
+
 func main() {
-	nodes := client.Status()
-	for _, node := range nodes {
-		fmt.Printf("ID=%v;Name=%v;Drain=%v\n", node.ID, node.Name, node.Drain)
+	flag.Parse()
+
+	if nomad == "" {
+		fmt.Println("nomad flag must be set.")
+		os.Exit(-1)
+	}
+
+	if hosts {
+		nodes := client.Status("http://" + nomad)
+		for _, node := range nodes {
+			fmt.Printf("ID=%v;Name=%v;Drain=%v\n", node.ID, node.Name, node.Drain)
+		}
 	}
 }
